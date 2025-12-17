1f5be1497b98b7aa6653654f8f3c120b91c68932
sndgfn <onikuzzaman113@gmail.com>
2025-12-18 02:09:02 +0600

    forth commit

--- solution.ts
+++ solution.ts
@@ -49,12 +49,34 @@ function filterByRating<T extends { rating: number }>(value: T[]) {
     return newArray
 }
 
-function filterActiveUsers<T extends { id: number, name: string, email: string, isActive: boolean }>(value :T[]) {
-    const newArray:T[]=[]
-        value.map((item) => {
-        if (item.isActive== true) {
+function filterActiveUsers<T extends { id: number, name: string, email: string, isActive: boolean }>(value: T[]) {
+    const newArray: T[] = []
+    value.map((item) => {
+        if (item.isActive == true) {
             newArray.push(item)
         }
     })
     return newArray
- }
+}
+
+
+interface Book {
+    title: string,
+    author: string,
+    publishedyear: number,
+    isAvailable: boolean
+}
+
+function printBookDetails<T extends { title: string, author: string, publishedyear: number, isAvailable: boolean }>(myBook: any) {
+    console.log(`Title:${myBook.title} , Author:${myBook.author},publishedyear:${myBook.publishedyear},${myBook.publishedyear},Available:${myBook.isAvailable}`)
+}
+
+function getUniqueValues(arr1: number[], arr2: number[]): number[] {
+    const newArray: number[] = [];
+    [...arr1, ...arr2].map((item) => {
+        if (!newArray.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
+}
